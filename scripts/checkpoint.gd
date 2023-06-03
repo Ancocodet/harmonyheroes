e62870830286c0fa70e251541a86fb0535159d8d
Ancocodet <ancoplays@gmail.com>
2023-06-03 17:22:50 +0200

    Use book for checkpoint

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -1,9 +1,18 @@
 class_name Checkpoint extends Area2D
 
 @export var id = 1
+@export var checked_atlas = Vector2(3, 1) 
+@onready var tilemap = $"../TileMap"
+
+var checked = false
 
 func _on_body_entered(_body):
-	GameManager.checkpoint_reached(self)
+	if !checked:
+		checked = true
+		GameManager.checkpoint_reached(self)
+		var area_position = Vector2(position.x, position.y)
+		var clicked_cell = tilemap.local_to_map(area_position)
+		tilemap.set_cell(0, clicked_cell, 0, checked_atlas)
 
 func get_id():
 	return id
